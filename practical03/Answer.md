e79a66603b116e4376e384ef7a406b586c6783f5
Boris Morin <145043616+BorisM202@users.noreply.github.com>
2023-10-09 18:42:27 +0100

    Add files via upload

---
+++ practical03/Answer.md
@@ -0,0 +1,8 @@
+### Printf Syntax
+
+```c++
+printf("Two ints %d %d and two floats %f %f\n", int1, int2, float1, float2);
+```
+
+### Exercices
+
---
+++ practical03/Area.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+/* Declare variables */
+    int i, N;
+    float value, h, a, b, pi;
+
+/* Initialize the value */
+    pi = atanf(1.0)/4.0;            // Definition of Pi
+    a = 0;
+    b = pi / 3;                     // First values of a and b
+    N = 12;                         // Number of iteration
+
+/* Compute the Sum */
+    h = (b - a) / (N - 1);          // Create 11 equidistants points
+
+    value = 2 * (tan(a) + tan(b));  
+    for (i=0; i < N; i++);          // for loop from 0 to N
+        value += tan(a+h*(i-1));
+    
+/* Print the values */
+    printf("The area under the curve is equal to %f \n", value);
+}
