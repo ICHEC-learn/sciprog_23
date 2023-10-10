dd8f1cb218b594851cf57a031327c64790f6d5b6
BorisM202 <145043616+BorisM202@users.noreply.github.com>
2023-10-10 20:19:32 +0100

    Fixed the code so it does compile and find the right values. But cannot print the full array

--- practical04/Tang.c
+++ practical04/Tang.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 
+// Global variables
+int N=12;
 float pi;
+// Functions
 float degtorad(float arg);
+
 
 int main(void) {
 
-/* Declare variables */
+// Declare variables
     int i;
     float degang, radang;
-    float Tan060[13];
+    float Tan060[N];
 
-/* Loop and store the results in an array */
+// Initialize variables
     pi = atanf(1.0)/4.0;
 
-    for (i; i<13; i++);
+// Loop and store the results in an array
+    for (i=0;i<N; i++); {
         degang = i * 5;
-        radang = degtorad(i); // use the function descibred below
-
+        radang = degtorad(degang); // use the function to convert deg to rad
         Tan060[i] = tan(radang); 
+        printf("Deg %f, Tan %f", degang, Tan060[i]);
+    }
 
-    printf("Deg %f, Tan %f\n");
     return 0;
 }
 
-/* Function to convert angle from degrees to radians */
+// Function to convert angle from degrees to radians
 float degtorad(float arg) {
     return( (pi * arg)/180.0 );
 }
