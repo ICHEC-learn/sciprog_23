dd02f4a757de81bcc59e8b89387e6dec0aac8fcd
BorisM202 <boris.morin@ucdconnect.ie>
2023-10-23 16:15:37 +0100

    Finished practical03

--- practical03/Area.c
+++ practical03/Area.c
@@ -3,26 +3,24 @@
 
 int main(void) {
 
-/* Declare variables */
-    int i, N;
-    float value, h, a, b, pi, log2;
-
-/* Initialize the value */
+/* Declare and Initialize variables */
     pi = atanf(1.0)/4.0;            // Definition of Pi
-    a = 0;
-    b = pi / 3;                     // First values of a and b
     N = 12;                         // Number of iteration
-    log2 = log(2);
+    double a = 0.0;                 // Lower part
+    double b = pi/3.0;              // Upper part
+    double width = (b-a)/(double)N; // Width
+    double value ;                  // Where to store the integrale
+    double x, y;
 
 /* Compute the Sum */
-    h = (b - a) / (N - 1);          // Create 11 equidistants points
+    value = tan(a) + tan(b);  
 
-    value = 2 * (tan(a) + tan(b));  
-
-    for (i=1; i < N; i++);          // for loop from 0 to N
-        value += tan(a+h*(i));
+    for (int i=1; i < N; i++);          // for loop from 0 to N
+        value += 2 * tan(a+width*i);    
     
 /* Print the values */
-    printf("The area under the curve is equal to %f \n", value/2);
-    printf("The value of log(2) is equal to : %f", log2);
+    printf("The area under the curve is equal to %.5f \n", value*width/2);
+    printf("The value of log(2) is equal to : %.5f", log2);
+
+    return 0;
 }
