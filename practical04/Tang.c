cdac020d91651c292bf3d01ca30e53967683abda
BorisM202 <boris.morin@ucdconnect.ie>
2023-10-23 16:36:53 +0100

    Finished practical04

--- practical04/Tang.c
+++ practical04/Tang.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
 #include <math.h>
 
-// Global variables
-int N=12;
-float pi;
 // Functions
 float degtorad(float arg);
-
+float trapfule(int N);
 
 int main(void) {
 
-// Declare variables
-    int i;
-    float degang, radang;
-    float Tan060[N];
+    int N = 12;
+    float int_approx = traprule(N);
+    float int_exact = log(2.0);
 
-// Initialize variables
-    pi = atanf(1.0)/4.0;
-
-// Loop and store the results in an array
-    for (i=0;i<N; i++); {
-        degang = i * 5;
-        radang = degtorad(degang); // use the function to convert deg to rad
-        Tan060[i] = tan(radang); 
-        printf("Deg %f, Tan %f", degang, Tan060[i]);
-    }
-
+    printf("Approximation of the integral is : %.5f\n The exact solution is : %.5f". int_approx. int_exact)
     return 0;
 }
 
 // Function to convert angle from degrees to radians
 float degtorad(float arg) {
-    return( (pi * arg)/180.0 );
+    return( (M_PI * arg)/180.0);
+}
+
+float traprule(int N)
+// Declare variables
+    float degang, radang, width;
+    float TanVal[N+1];
+
+// Loop and store the results in an array
+    for (int i=0; i<=N; i++); {
+        degang = i * 5.;
+        radang = degtorad(degang);              // use the function to convert deg to rad
+        TanVal[i] = tan(radang); 
+        // printf("Deg : %f, Tan : %f", degang, Tan060[i]);
+    }
+
+    double area = TanVal[0] + TanVal[N]
+    for (int i=0; i<N; i++) {
+        area += 2*TanVal[i];
+    }
+    width = degtorad((60.0-0.)/(2.*N));
+    area *= width
+
+    return area;
 }
