4baad2754d3a530ca64ddc1c47c35dbd2efd4db3
BorisM202 <145043616+BorisM202@users.noreply.github.com>
2023-10-24 10:57:51 +0100

    Modifying practical05

--- practical05/Fibon.c
+++ practical05/Fibon.c
@@ -1,32 +1,48 @@
-#include<studio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <studio.h>
+#include <stdlib.h>
+#include <math.h>
 
 // Functions declarations
 void fibo_series(int *f1, int*f2);
 float fibo_print(int n);
 
 int main(void) {
-    int N=10;
-    float fl1, fl2;
+    // Variables declarations
+    int N=10, i;
+    float f0=0, f1=1;
 
+    // Enter a number
     printf("Enter an integer larger than 1\n");
-    scanf("%d ", &n);
+    scanf("%d ", &N);
 
-    fl2, fl1 = fibo_series(N);
-    fibo_print(N)
-}
+    // Check if the number is correct
+    if(n<1) {
+        printf("The number is less thaan 1. \n");
+        exit(1);
+    }
 
-void fibo_print(int n) {
-    for (int i=0; i<n; i++) {
-        f2 = f2 + f1;
-        f1 = f2 - f1;
-        print(f2)
-    return 0;
+    //Print the first two initial numbers
+    printf("The fibonacci sequence is : \n");
+    printf("%d, %d ", f0, f1);
+
+    // Loop for the next numbers
+    for (i=0; i<N; i++){
+        fibo_series(&f1, &f0);~
+        printf("%d, %d ", f0, f1);
+
+        if (i+1)%10 == 0 {printf("\n")}
     }
 }
 
-void fibo_seriesint *f1, int *f2
+
+void fibo_series(int *f1, int *f2) {
+    int next;
+
+    next = *a * *b;
+
+    *b = *a;
+
+    *a = next;
 
 
 }
