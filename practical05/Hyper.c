d03d6b5fdf5f69e4a4d2130d20c8a5222c8728c5
BorisM202 <145043616+BorisM202@users.noreply.github.com>
2023-10-24 11:46:28 +0100

    Modifying practical05

--- practical05/Hyper.c
+++ practical05/Hyper.c
@@ -1,35 +1,65 @@
-#include <studio.h>
+#include <stdio.h>
 #include <math.h>
 
-arctanh1(double x, int N);
-arctanh2(double x);
+// Functions declarations
+arctanh1(const double x, const double delta);
+arctanh2(const double x);
 
 
 int main(void) {
 
-    float X;
-    float iter = (0.9 - (-0.9))/0.01;
-    float T1[(int)iter], T2[(int)inter];
+    // Variables declarations
+    // float iter = (0.9 - (-0.9))/0.01;
+    //double T1[(int)iter], T2[(int)inter];
+    double delta, x;
+    int lenght=1000;
+    int N = 1000
+    double T1[N], T2[N];
 
-    printf("Enter a real positive number\n");
-    scanf("%3f ", &X);
+    // User Input for delta
+    printf("Enter the precision for the MacLaurin Series\n");
+    scanf("%1f ", &delta);
+
+    int j=0; // array index
+    x = -0.9 ;
+    while (x<=0.9 && j < lenght) {
+        tan1[j] = arctanh1(x, delta);
+        tan2[j] = arctanh2(x)
+        print("The difference at x=%lf between them is %.10lf.
+    }
+
+
+
+
+
+
+    return 0;
 
 }
 
-arctanh1(double x, int N){
+arctanh1(const double x, const double delta) {
     double sum = 0;
-    double delta = 0;
-    while (delta > 0.01) {
-        delta = (pow(x,2*N+1))/(2*N+1);
-        sum += delta;
-    }
+    double elem, val;
+    int n;
+
+    do {
+        val = 2*n+1;
+        elem = pow(x,val)/val;
+        sum += elem;
+        n++;
+
+    } while(fabs(elem) > delta)
+
     return sum;
 }
 
-arctanh2(double x) {
-
+arctanh2(const double x) {
     double value;
     value = 0.5*(log(1+x)-log(1-x));
     return value;
 
 }
+
+
+
+
