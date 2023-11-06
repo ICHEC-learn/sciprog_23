d2b960743f72a298a37e6d8e7853e73b4143f1e7
BorisM202 <145043616+BorisM202@users.noreply.github.com>
2023-11-06 18:57:31 +0000

    Change Practical03

--- practical03/Area.c
+++ practical03/Area.c
@@ -3,24 +3,31 @@
 
 int main(void) {
 
-/* Declare and Initialize variables */
-    pi = atanf(1.0)/4.0;            // Definition of Pi
-    N = 12;                         // Number of iteration
-    double a = 0.0;                 // Lower part
-    double b = pi/3.0;              // Upper part
-    double width = (b-a)/(double)N; // Width
-    double value ;                  // Where to store the integrale
-    double x, y;
+    // Declare and Initialize variables
+    int N = 12;                             // Number of iteration
+    int i = 0;
+    double x_i = 0.0;                       // Lower part
+    double x_f = M_PI / 3.0;                // Upper part
+    double width = (x_f - x_i)/(double)N;   // Width
+    double value;                           // Where to store the integrale
+    double x, y = 0.;
 
-/* Compute the Sum */
-    value = tan(a) + tan(b);  
+    // Compute the Sum
+    value = tan(x_i) + tan(x_f);
 
-    for (int i=1; i < N; i++);          // for loop from 0 to N
-        value += 2 * tan(a+width*i);    
-    
-/* Print the values */
-    printf("The area under the curve is equal to %.5f \n", value*width/2);
-    printf("The value of log(2) is equal to : %.5f", log2);
+    // Loop over the N values
+    for (i = 1; i < N; i++) {                // for loop from 0 to N
+        x = x_i + width * i;
+        y = tan(x);
+        value += 2 * y;
+    }
+    // Print the values
+    double integral_approx = value * width * 0.5;
+    double integral_exact = log(2.0);
+
+    printf("The area under the curve is equal to : %lf  \n", integral_approx);
+    printf("The value of log(2) is equal to : %lf \n", integral_exact);
+    printf("The difference between the approximation and the exact value is : %lf \n", integral_approx - integral_exact);
 
     return 0;
 }
